35d1de8401496548a995e3337e5b2e1aa9fa175a
Uniquegf <90412360+Uniquegf@users.noreply.github.com>
2023-09-05 09:02:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,79 @@
-//card number input.....
-const ErrorCard = document.querySelector('#error_inputNum');
+//card name input.....
+const cardName = document.querySelector('#card-name');
+//card name error....
+const ErrorText = document.querySelector('#error_text');
+//cardholder number input.....
+const CardNumber = document.querySelector('#card-number');
+//cardholder number error.....
+const ErrorNumber = document.querySelector('#error_number');
+//Exp date(MM) input..
+const MM = document.querySelector('#mm');
+//Exp date(MM) input...
+const YY = document.querySelector('#yy');
+// Exp date(MM/YY)......
+const ErrorMMYY = document.querySelector('#error_Exp');
+//CVC input.....
+const Cvc = document.querySelector('cvc');
+//CVC error input.....
+const ErrorCvc = document.querySelector('error_cvc');
+// submit btn....
+const SubmitBtn = document.querySelector('#btn_form');
+//modal
+const Modal = document.querySelector('.modle-card');
+const BtnModal = document.querySelector('#btn_modle');
+const Form = document.querySelector('.form');
 
-const ErrCardNUm = document.querySelector('#error_num');
-const Exp_Month = document.querySelector('#MM');
-const Exp_Year = document.querySelector('#YY');
-const ErrorExp = document.querySelector('#error_exp');
-const ErrorCvIn = document.querySelector('#error_cvInput');
-const ErrorCvc = document.querySelector('#error_cvc');
-const SubBtn = document.querySelector('.Sub_btn');
-const ErrorClass = document.querySelector('.hidden');
+SubmitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
 
-let isValid = false;
+  const Name = cardName.value;
 
-ErrorCard.addEventListener('input', function () {
-  if (+ee.value > 31 || +inputDay.value < 0) {
-    ErrorCard.style.border = '1px solid hs1(0, 100%, 67%)';
-    ErrCardNUm.style.color = 'hs1(0,100%, 67%)';
-    isValid = false;
-    return;
+  const NameCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (Name === '' || !NameCheck.test(Name)) {
+    cardName.style.border = '1px solid red';
+    ErrorText.textContent = 'Valid name required';
+    ErrorText.style.color = 'red';
   } else {
-    isValid = true;
-    ErrorCard.style.border = '';
-    ErrCardNUm.style.color = '';
+    ErrorText.textContent = '';
+    cardName.value = '';
+    Form.style.display = 'none';
+    Modal.style.display = 'block';
   }
 
-  if (+inputDay.value === 0) {
-    errorDay.textContent = 'this field id required';
-    inputDay.style.border = '1px solid hs1(0,100%, 67%)';
-    labelDay.style.color = 'hs1(0, 100%, 67%)';
-    isValid = false;
-    return;
+  //Check if the input is a string.
+  if (typeof CardNumber !== 'string') {
+    CardNumber.style.border = '1px solid red';
+    ErrorNumber.textContent = 'Valid number required';
+    ErrorNumber.style.color = 'red';
+    return false;
   } else {
-    isValid = true;
-    errorDay.textContent = '';
-    inputDay.style.border = '';
-    labelDay.style.color = '';
+    ErrorNumber.textContent = '';
+    CardNumber.value = '';
   }
+  //Check if the input is a 12 characters long.
+  if (CardNumber.length !== 12) {
+    return false;
+  } else {
+    console.log('Wrong format, numbers only');
+  }
+  //Check if the input contains any letters.
+  if (/[a-zA-Z]/.test(CardNumber)) {
+    return false;
+  } else {
+    console.log('Wrong format, numbers only');
+  }
+  // The input is valid.
+  // ErrorNumber.textContent = '';
+  // CardNumber.value = '';
+  return true;
 });
 
-SubBtn.addEventListener('click', function () {});
-tg;
+BtnModal.addEventListener('click', () => {
+  Modal.style.display = 'none';
+  Form.style.display = 'block';
+});
+
+window.addEventListener('load', () => {
+  cardName.classList.remove('error');
+});
